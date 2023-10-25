a58e476a76c7f414a222f4bcab52ede16e4591de
Akhilendra01 <akhilendra1604@gmail.com>
2023-10-25 13:06:02 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+require('dotenv').config();
+
+const express=require('express');
+const mongoose=require('mongoose');
+
+const cors=require('cors');
+
+const multer=require('multer');
+const {storage}=require('./cloudinary');
+
+const Buyer=require('./models/buyer');
+
+const upload=multer({storage});
+
+const app=express();
+// app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(()=>{
+    console.log('Connected to MongoDB');
+})
+.catch(err=>{
+    console.log('Error', err);
+});
+
+
+app.post('/api/add_buyer', upload.single('screenshot'), async(req, res)=>{
+    console.log(req.body);
+    const buyer=new Buyer({
+        name: req.body.name,
+        email: req.body.email,
+        mobile: req.body.mobile,
+        college: req.body.college,
+        year: req.body.year,
+        scholar_no: req.body.scholar_no,
+        address: req.body.address,
+        quantity: req.body.quantity,
+        amount: req.body.amount,
+        img_url: req.file.path,
+    });
+
+    await buyer.save();
+    res.send({"status": "200"});
+});
+
+
+
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Listening to port ${process.env.PORT}`);
+});
+
---
+++ models/buyer.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const buyerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+  },
+  college: {
+    type: String,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  scholar_no: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  img_url: {
+    type: String,
+    required: true,
+  },
+});
+
+const Buyer = mongoose.model("buyer", buyerSchema);
+
+module.exports = Buyer;
